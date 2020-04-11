b2c674beaf7fdf77f6e8528e5e5a6fac3e93a536
Marat Dospolov <marat.dospolov@gmail.com>
2020-04-11 20:22:56 +0300

    cv tab title

--- src/templates/cv-template.js
+++ src/templates/cv-template.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import CV from '../components/CV'
+import { useSiteMetadata } from '../hooks'
 
-const CvTemplate = () => (
-  <Layout title="Marat Dospolov | CV" className="bg-black">
-    <CV />
-  </Layout>
-)
+const CvTemplate = () => {
+  const { author } = useSiteMetadata()
+
+  return (
+    <Layout title={`${author.name} | CV`} className="bg-black">
+      <CV />
+    </Layout>
+  )
+}
 
 export default CvTemplate
