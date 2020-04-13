5791dd0e963e184d4ca6119cdcd985f86fb697b1
Marat Dospolov <marat.dospolov@gmail.com>
2020-04-13 17:59:31 +0300

    offline disabled

--- gatsby-config.js
+++ gatsby-config.js
@@ -184,7 +184,6 @@ module.exports = {
         icon: 'static/photo.jpg'
       }
     },
-    'gatsby-plugin-offline',
     'gatsby-plugin-catch-links',
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-postcss`
